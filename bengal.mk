b918f921540043d26356c9e46033e025206c4f61
josexda <josegustavo6@protonmail.com>
2024-08-28 08:47:13 -0300

    sm6225-commom: adapt for yaap
    Change-Id: If71d3da0cb6cd23c26b6aaded57870782d4e6c53

--- bengal.mk
+++ bengal.mk
@@ -213,7 +213,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
